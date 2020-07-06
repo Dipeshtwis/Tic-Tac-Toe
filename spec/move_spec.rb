c7467b6a3c65e88876b23ca9b01b406fd9ca905b
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-07-06 18:28:47 +0530

    Add the test for add player

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -4,8 +4,8 @@ require_relative '../lib/player.rb'
 describe Move do
 	let(:player) {Player.new('Iggy', 'X')}
 	let(:move) {Move.new}
-	describe '#add_player' do
-		it 'add the player' do
+	describe "#add_player" do
+		it "add the player" do
 			expect(move.add_player(player)).to eql([])
 		end
 	end
