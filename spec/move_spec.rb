e88cde019d0108d9d714bdbfb5dd0b14f3810b8a
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-07-06 21:31:53 +0530

    Add test for number_present

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -15,4 +15,23 @@ describe Move do
 			expect(move.add_move(player.name, 6)).to eql([6])
 		end
 	end
+
+	describe "#number_present" do
+		context "If number is between 1 to 9" do
+			it "return false if number is present" do
+				expect(move.number_present(2)).to eql(false)
+			end
+
+			it "return true if number is not present" do
+				expect(move.number_present(10)).to eql(true)
+			end
+		end
+
+		context "If number is present in the array" do
+			let(:player_number){'Iggy : [1, 2]'}
+			it "return true if number is already present in the array" do
+				expect(player_number.number_present("2")).to eql(true)
+			end
+		end
+	end
 end
